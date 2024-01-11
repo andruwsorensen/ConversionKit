1d723f7c6f2f9861be163ba4f5209d53a225ca45
andruw-sorensen <andruw.sorensen@gmail.com>
2024-01-11 09:48:08 -0700

    Initial Commit

---
+++ ConversionKit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConversionKit
+//
+//  Created by Andruw Sorensen on 1/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ConversionKit/ConversionKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConversionKitApp.swift
+//  ConversionKit
+//
+//  Created by Andruw Sorensen on 1/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ConversionKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
